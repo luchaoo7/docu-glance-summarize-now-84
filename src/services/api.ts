d85db83287377a617cd20ff38a765ce6b59f20b6
Daniel Brown <lucha0075.db@gmail.com>
2025-08-23 12:04:15 +0100

    add separate endpoins to make request for asks and upload v3

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import { supabase } from '../lib/supabase/client'
 // Interface for the request payload
 interface DocumentProcessRequest {
   document: File;
@@ -28,11 +29,11 @@ export const processDocument = async (document: File, questions: string[]) => {
     });
 
 
-    console.log('Sending document and questions to backend:', {
-      documentName: document.name,
-      documentSize: document.size,
-      questions: questions
-    });
+    // console.log('Sending document and questions to backend:', {
+      // documentName: document.name,
+      // documentSize: document.size,
+      // questions: questions
+    // });
 
     return response;
     // Simulate API call delay
@@ -47,3 +48,41 @@ export const processDocument = async (document: File, questions: string[]) => {
     throw error;
   }
 };
+
+
+export async function uploadDocument(document: File) {
+  const session = await supabase.auth.getSession()
+  const token = session.data.session?.access_token
+
+  const formData = new FormData()
+  formData.append('document', document);
+
+  // const res = await axios.post('http://localhost:8000/upload', formData, {
+  const res = await axios.post('/api/upload/v3/', formData, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+      'Content-Type': 'multipart/form-data'
+    }
+  })
+
+  return res.data
+}
+
+export async function askQuestion(fileId: string, question: string) {
+  const session = await supabase.auth.getSession()
+  const token = session.data.session?.access_token
+
+  // const res = await axios.get('http://localhost:8000/ask', {
+  const res = await axios.get('/api/ask/v3/', {
+    params: {
+      file_id: fileId,
+      q: question
+    },
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  })
+
+  return res.data.answer
+}
+
