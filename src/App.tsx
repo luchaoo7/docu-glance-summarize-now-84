9b38e54bd343d1101d68f0cc63d3fbe11da54952
Daniel Brown <lucha0075.db@gmail.com>
2025-08-23 12:04:53 +0100

    introduce loging and sigup

--- src/App.tsx
+++ src/App.tsx
@@ -4,26 +4,74 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import './i18n'; // Import i18n configuration
+
+import { useEffect, useState } from "react";
+import { supabase } from '@/lib/supabase/client'
+
+import AuthForm from "./components/AuthForm";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-import './i18n'; // Import i18n configuration
 
 const queryClient = new QueryClient();
 
-const App = () => (
-  <QueryClientProvider client={queryClient}>
+const App = () => {
+
+  const [session, setSession] = useState<any>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Get current session on load
+    supabase.auth.getSession().then(({ data }) => {
+      console.log("Data session is: ")
+      console.log(data.session)
+
+      setSession(data.session);
+      setLoading(false);
+    });
+
+    // Subscribe to future changes
+    const { data: listener } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+
+    console.log("Data session is: ")
+    console.log(session)
+
+
+
+    return () => {
+      listener.subscription.unsubscribe();
+    };
+  }, []);
+
+  if (loading) return <div>Loading...</div>;
+
+  return (<QueryClientProvider client={queryClient}>
     <TooltipProvider>
       <Toaster />
       <Sonner />
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+        {session ? (
+          <Routes>
+            {/* Protected routes when logged in */}
+
+            <Route path="/" element={<Index />} />
+            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        ) : (
+          // Public routes
+          <Routes>
+            <Route path="*" element={<AuthForm />} />
+          </Routes>
+
+        )}
+
       </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
-);
+  );
+}
 
 export default App;
